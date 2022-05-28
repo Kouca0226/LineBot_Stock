06281b5c2a19add15ce486dace490d5d0c54b141
Kouca0226 <102953326+Kouca0226@users.noreply.github.com>
2022-05-28 18:20:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -42,6 +42,15 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
 
+import re
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = event.message.text
+    if re.match("你是誰",message):
+        line_bot_api.reply_message(event.reply_token,TextSendMessage("才不告訴你勒~~"))
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(message))
+
 #主程式
 import os 
 if __name__ == "__main__":
