55f15af95100415dae6ef2cd9069b158785b8747
Kouca0226 <102953326+Kouca0226@users.noreply.github.com>
2022-05-31 16:50:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ def handle_message(event):
                                 QuickReplyButton(action=MessageAction(label="三大法人", text="三大法人買賣超 " + message[5:]))
                             ]))
         line_bot_api.reply_message(event.reply_token, flex_message)
-    elif '個股資訊 ' in message:
-        line_bot_api.reply_message(event.reply_token, message)
+    elif '#' in message:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message[1:]))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage("錯誤指令\n請輸入「help」查詢"))
 
