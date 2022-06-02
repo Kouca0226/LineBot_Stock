5bc2b1ae2a23af6bdcde3f8f7fe82bde577b1328
Kouca0226 <102953326+Kouca0226@users.noreply.github.com>
2022-06-03 07:12:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('rBdXfRQqtY6avy9CJ3Ttenas9mkSoBGquP/qISdSUDfaDIndkKWlDRe4uvz2T5PKJ3f9EbfilzJB7n6Mn5oF//xZtBF5KQEhOdFFuUlgaqJ8LZ1McPOuCzrB9hOZuqLeT0cF+5zd1ZdnH+2gUuXsrgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('f40eb4fe030cdece35f592631b9e184a')
-line_bot_api.push_message('U82a41664b4a103c31ae2be046358f484', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U82a41664b4a103c31ae2be046358f484', TextSendMessage(text='程式運行中\n在股票名稱或代碼前加上「股票 」即可查詢\n(ex.股票 台積電)'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -41,55 +41,9 @@ import re
 def handle_message(event):
     message = text = event.message.text
     if "股票 " in message:
-        buttons_template_message = TemplateSendMessage(
-        alt_text = "股票資訊",
-        template=CarouselTemplate(
-            columns=[
-                CarouselColumn(
-                    title = message + " 股票資訊",
-                    text ="請點選想查詢的股票資訊",
-                    actions =[
-                        MessageAction(
-                            label= message[3:] + " 個股資訊",
-                            text= "個股資訊 " + message[3:]),
-                        MessageAction(
-                            label= message[3:] + " 個股新聞",
-                            text= "個股新聞 " + message[3:])
-                    ]
-                ),
-                CarouselColumn(
-                    title = message[3:] + " 股票資訊",
-                    text ="請點選想查詢的股票資訊",
-                    actions =[
-                        MessageAction(
-                            label= message[3:] + " 最新分鐘圖",
-                            text= "最新分鐘圖 " + message[3:]),
-                        MessageAction(
-                            label= message[3:] + " 日線圖",
-                            text= "日線圖 " + message[3:]),
-                    ]
-                ),
-                CarouselColumn(
-                    title = message[3:] + " 股利資訊",
-                    text ="請點選想查詢的股票資訊",
-                    actions =[
-                        MessageAction(
-                            label= message[3:] + " 平均股利",
-                            text= "平均股利 " + message[3:]),
-                        MessageAction(
-                            label= message[3:] + " 歷年股利",
-                            text= "歷年股利 " + message[3:])
-                    ]
-                ),
-            ]
-        )
-    )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)
-    
-    elif '大戶籌碼 ' in message:
-        flex_message = TextSendMessage(text="請選擇要顯示的買賣超資訊",
+        flex_message = TextSendMessage(text="請選擇相關資訊",
                                 quick_reply=QuickReply(items=[
-                                QuickReplyButton(action=MessageAction(label="最新法人", text="最新法人買賣超 " + message[5:])),
+                                QuickReplyButton(action=MessageAction(label="及時股價", text="及時股價 " + price(message[3:]))),
                                 QuickReplyButton(action=MessageAction(label="歷年法人", text="歷年法人買賣超 " + message[5:])),
                                 QuickReplyButton(action=MessageAction(label="外資", text="外資買賣超 " + message[5:])),
                                 QuickReplyButton(action=MessageAction(label="投信", text="投信買賣超 " + message[5:])),
@@ -97,12 +51,20 @@ def handle_message(event):
                                 QuickReplyButton(action=MessageAction(label="三大法人", text="三大法人買賣超 " + message[5:]))
                             ]))
         line_bot_api.reply_message(event.reply_token, flex_message)
-    elif '#' in message:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(get_stock(message[1:])))
-    elif '個股資訊 ' in message:
+    elif '及時股價 ' in message:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(get_stock(get_stockid(message[5:]))))
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage("錯誤指令\n請輸入「help」查詢"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("錯誤指令"))
+
+def price(inp):
+    try:
+        try:
+            out = get_stock(get_stockid(inp))
+        except:
+            out = get_stock(inp)
+    except:
+        return "查無此股票"
+    return out
 
 def get_stock(stockn):
     url = "https://tw.stock.yahoo.com/quote/" + stockn
