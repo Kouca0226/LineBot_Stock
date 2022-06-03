604b07d1ee99a04c0de32c15c8f05f21fd27e1d6
Kouca0226 <102953326+Kouca0226@users.noreply.github.com>
2022-06-03 15:25:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -113,5 +113,6 @@ def get_stockid(name):
 #主程式
 import os 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    while(1):
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
